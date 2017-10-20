4f1850f3f001c818bebcdaa0a51bc5783c855251
shiota yoshiki <shiotyan317@gmail.com>
2017-10-20 23:53:42 +0900

    create sessions
    sessionの作成
    ヘッダーにログインボタンの記述

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :users
-	root 'static_pages#home' # => root_path
+  root 'static_pages#home' # => root_path
 
-	  get  '/signup',  to: 'users#new'
-	  post '/signup',  to: 'users#create'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
