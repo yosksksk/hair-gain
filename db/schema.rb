68046ce2a21a13b263eb1c34cd549b477f3d9f29
shiota yoshiki <shiotyan317@gmail.com>
2017-10-20 23:57:17 +0900

    forget user password
    userにパスワードをつけるのを忘れていたのでつけました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019174949) do
+ActiveRecord::Schema.define(version: 20171020145403) do
 
   create_table "users", force: :cascade do |t|
     t.integer "user_type"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171019174949) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
