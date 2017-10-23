ab99456b289879c12415646261794d8aa6d5a31c
Yoshiki Nishioka <yoshiki.nishioka.kobe@gmail.com>
2017-10-23 15:37:13 +0900

    on the way to product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/new'
+  get  '/product',  to: 'products#new'
+  post '/product',  to: 'products#create'
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
