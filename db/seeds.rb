52e26f946d4c08772a8282a2b53add36fc1d2daf
shiota yoshiki <shiotyan317@gmail.com>
2017-10-23 19:46:37 +0900

    create models and test seed
    	order		order information
    	orderItem		order item information
    	orderStatus	progress or complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create! id: 1, name: "Banana", price: 1200, introduction: "test",company:"banaaaaa",stock:32,image:"void_image",category:"fruits"
+Product.create! id: 2, name: "Apple", price: 100, introduction: "apple_test",company:"aaaaapple",stock:12,image:"apple_void_image",category:"fruits"
+Product.create! id: 3, name: "rwar", price: 9000, introduction: "test",company:"90aa",stock:32,image:"void_image",category:"test"
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
