b711293d53fd07d5ef30f21c22c413e11e9c26d3
Yoshiki Nishioka <yoshiki.nishioka.kobe@gmail.com>
2017-10-23 17:20:43 +0900

    add image and category

--- Gemfile
+++ Gemfile
@@ -30,6 +30,10 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'carrierwave'
+
+gem 'rmagick', '~> 2.16.0'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019073137) do
+ActiveRecord::Schema.define(version: 20171023081513) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20171019073137) do
     t.string "company"
     t.integer "stock"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false 
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
