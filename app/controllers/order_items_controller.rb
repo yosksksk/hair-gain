e41b7b2b83c5c83ee192b48fcabdd3c3e523437c
shiota yoshiki <shiotyan317@gmail.com>
2017-10-23 20:37:17 +0900

    Enroute commit

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,7 +2,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
-    @order.save
+    @order.save!
     session[:order_id] = @order.id
   end
 
